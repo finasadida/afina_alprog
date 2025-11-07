0c11453c93d5f23676b500c8e2b7ed355121e4b2
finasadida <finasadida@gmail.com>
2025-11-07 09:19:26 +0800

    Create pert1.py

---
+++ pert1.py
@@ -0,0 +1,18 @@
+# saya sedang belajar phyton
+# coba dibaca dari kiri kekanan
+# code dibaca dari atas ke bawah
+
+# cara untuk memindahkan saty baris
+# arahkan kurser pada baris yang ingin di pindahkan
+# tekan tombol alt, lalu tekan tombol arah bawah atau atas
+
+# cara untuk mengcopy satu baris full
+# sama dengan yang diatas, namun ditambahkan tekan tombol shift
+
+print("Nama Saya Fina")
+print("Saya Mahasiswa")
+print("tapi main roblox")
+
+x = 5
+y = 6
+print(x + y)
